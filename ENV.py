5a4cb4d703e837216aa0f931946d6ed07708c870
HuanjunWang <153147512@qq.com>
2018-04-26 15:52:11 +0800

    bug fix

--- ENV.py
+++ ENV.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 
 class ENV(object):
@@ -10,32 +11,42 @@ class ENV(object):
     def __init__(self):
         self.data = None
         self.index = None
+        self.X = None
+        self.Y = None
+        self.TSize = None
         self.load_data()
         self.reset()
 
     def load_data(self):
         with open(self.DELAY_FILE, "rb") as f:
             self.data = pickle.load(f)
-            print("Total PGSL messages:", len(self.data))
+            self.X = []
+            self.Y = []
+            for i in range(ENV.STATE_LEN + 100000, len(self.data)):
+                self.X.append(self.data[i - ENV.STATE_LEN - ENV.MARGIN:i-ENV.MARGIN])
+                self.Y.append(self.data[i])
+            self.TSize = len(self.X)
+
+            print("Total PGSL messages:", len(self.data), "Training Samples:", self.TSize)
 
     def reset(self, index=None):
         if index is None:
-            self.index = self.WINDOW + self.STATE_LEN
+            self.index = self.MARGIN + self.STATE_LEN
         else:
-            if index < self.WINDOW + self.STATE_LEN or index > len(self.data):
+            if index < self.MARGIN + self.STATE_LEN or index > len(self.data):
                 raise IndexError("Index out our delay data range")
             self.index = index
-        return self.data[self.index - self.WINDOW - self.STATE_LEN:self.index - self.WINDOW]
+        return self.data[self.index - self.MARGIN - self.STATE_LEN:self.index - self.MARGIN]
 
     def step(self, advance):
-        if -self.WINDOW <= advance - self.data[self.index] <= self.WINDOW:
+        if np.abs(advance - self.data[self.index]) <= self.WINDOW:
             reward = 1
         else:
             reward = 0
         self.index += 1
         end = (self.index == len(self.data))
 
-        return self.data[self.index - self.WINDOW - self.STATE_LEN:self.index - self.WINDOW], reward, end
+        return self.data[self.index - self.MARGIN - self.STATE_LEN:self.index - self.MARGIN], reward, end
 
 
 if __name__ == "__main__":
@@ -51,6 +62,7 @@ if __name__ == "__main__":
                 break
         print("[Fix Adv]Messages:%d ADV:%d Total Reward:%d" % (number, adv, total_reward))
 
+
     env = ENV()
     run_with_fix_advance(env, adv=-2)
     run_with_fix_advance(env, adv=-1)
@@ -63,4 +75,3 @@ if __name__ == "__main__":
     run_with_fix_advance(env, adv=6)
     run_with_fix_advance(env, adv=7)
     run_with_fix_advance(env, adv=8)
-
