ab9678f71a03a282e736910b8d927a6199b67ade
Norden LIU <norden.liu@gmail.com>
2018-04-27 15:56:13 +0800

    arima finish

--- arima.py
+++ arima.py
@@ -61,21 +61,23 @@ ax2 = fig.add_subplot(212)
 fig = sm.graphics.tsa.plot_pacf(dta2,lags=40,ax=ax2)
 
 
-#arma_mod20 = sm.tsa.ARMA(dta2.astype('float64'),(2,0)).fit()
-#print(arma_mod20.aic,arma_mod20.bic,arma_mod20.hqic)
-#arma_mod01 = sm.tsa.ARMA(dta2.astype('float64'),(0,1)).fit()
-#print(arma_mod01.aic,arma_mod01.bic,arma_mod01.hqic)
+
+
 arma_mod21 = sm.tsa.ARMA(dta2.astype('float64'),(2,1)).fit()
-#print(arma_mod21.aic,arma_mod21.bic,arma_mod21.hqic)
-#arma_mod30 = sm.tsa.ARMA(dta2.astype('float64'),(3,0)).fit()
-#print(arma_mod30.aic,arma_mod30.bic,arma_mod30.hqic)
-#arma_mod31 = sm.tsa.ARMA(dta2.astype('float64'),(3,1)).fit()
-#print(arma_mod31.aic,arma_mod31.bic,arma_mod31.hqic)
+arma_mod20 = sm.tsa.ARMA(dta2.astype('float64'),(2,0)).fit()
+print(arma_mod20.aic,arma_mod20.bic,arma_mod20.hqic)
+arma_mod01 = sm.tsa.ARMA(dta2.astype('float64'),(0,1)).fit()
+print(arma_mod01.aic,arma_mod01.bic,arma_mod01.hqic)
+print(arma_mod21.aic,arma_mod21.bic,arma_mod21.hqic)
+arma_mod30 = sm.tsa.ARMA(dta2.astype('float64'),(3,0)).fit()
+print(arma_mod30.aic,arma_mod30.bic,arma_mod30.hqic)
+arma_mod31 = sm.tsa.ARMA(dta2.astype('float64'),(3,1)).fit()
+print(arma_mod31.aic,arma_mod31.bic,arma_mod31.hqic)
 
 
+#mod21-->#173.32772855124503 186.738384687 178.765262518
 #174.55178448720673 185.280309396 178.901811661
 #206.098776562232 214.145170244 209.361296942
-#mod21-->#173.32772855124503 186.738384687 178.765262518
 #174.03232305944348 187.442979195 179.469857026
 #175.22216116088097 191.314948524 181.747201921
 
@@ -98,18 +100,54 @@ ax = fig.add_subplot(111)
 fig = qqplot(resid, line='q', ax=ax, fit=True)
 
 #Q/Jung-Box
-r,q,p = sm.tsa.acf(resid.values.squeeze(), qstat=True)
-data = np.c_[range(1,41), r[1:], q, p]
-table = pd.DataFrame(data, columns=['lag', "AC", "Q", "Prob(>Q)"])
+#r,q,p = sm.tsa.acf(resid.values.squeeze(), qstat=True)
+#data = np.c_[range(1,41), r[1:], q, p]
+#table = pd.DataFrame(data, columns=['lag', "AC", "Q", "Prob(>Q)"])
 #print(table.set_index('lag'))
 
 #predict
 
-predict_dta = arma_mod21.predict(108, 208, dynamic=True)
-print(predict_dta)
+predict_dta = arma_mod21.predict(58, 208, dynamic=True)
+#print(predict_dta)
+#print(dta2)
+
+fig = plt.figure(figsize=(12,8))
+ax1=fig.add_subplot(311)
+dta2.plot(ax=ax1)
+ax2 = fig.add_subplot(312)
+predict_dta.plot(ax=ax2)
+
+
+ax3 = fig.add_subplot(313)
+
+
+diff_shift_ts = diff1.shift(1)
+diff_recover = predict_dta.add(diff_shift_ts[0])
+diff_recover.dropna(inplace=True)
+print(diff_recover)
+diff_recover.plot(ax=ax3)
 
 
 fig, ax = plt.subplots(figsize=(12, 8))
 ax = dta2.loc[dta2.index[0:]].plot(ax=ax)
 fig = arma_mod21.plot_predict(108, 208, dynamic=True, ax=ax, plot_insample=False)
 plt.show()
+
+
+def getReward(actual,prediction,s):
+    if(np.abs(actual - prediction) <=s):
+        return 1
+    else:
+        return 0
+
+sum=0
+for i in range(58,107):
+    sum=sum+getReward(dta2[0][i],diff_recover[i-58],2)
+print("Limition=2,The rewards is:%d"%sum)
+print("The result is:%f"%(sum/(107-58)))
+
+sum=0
+for i in range(58,107):
+    sum=sum+getReward(dta2[0][i],diff_recover[i-58],1)
+print("Limition=1,The rewards is:%d"%sum)
+print("The result is:%f"%(sum/(107-58)))
