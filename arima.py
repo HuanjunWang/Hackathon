0c54948f4b719040be01a515f89e108e8f8880fe
Norden LIU <norden.liu@gmail.com>
2018-04-27 13:23:51 +0800

    fix problem

--- arima.py
+++ arima.py
@@ -26,7 +26,9 @@ import matplotlib.pyplot as plt
 #11999,9390,13481,14795,15845,15271,14686,11054,10395]
 
 dta2=pd.read_csv('delay_data_test.csv',header=None)
-dta2.index = pd.Index(sm.tsa.datetools.dates_from_range('1000',length=len(dta2)))
+#dta2.index = pd.Index(sm.tsa.datetools.dates_from_range('1000m1',length=len(dta2)))
+pd.to_datetime(dta2.index, unit='ms')
+dta2.index = pd.to_datetime(dta2.index, unit='ms')
 #dta2.index = pd.Index(range(0, len(dta2)))
 dta2.plot(figsize=(12,8))
 
@@ -59,12 +61,55 @@ ax2 = fig.add_subplot(212)
 fig = sm.graphics.tsa.plot_pacf(dta2,lags=40,ax=ax2)
 
 
-#arma_mod20 = sm.tsa.ARMA(dta3,(7,0)).fit()
+#arma_mod20 = sm.tsa.ARMA(dta2.astype('float64'),(2,0)).fit()
 #print(arma_mod20.aic,arma_mod20.bic,arma_mod20.hqic)
-#arma_mod30 = sm.tsa.ARMA(dta3,(0,1)).fit()
+#arma_mod01 = sm.tsa.ARMA(dta2.astype('float64'),(0,1)).fit()
+#print(arma_mod01.aic,arma_mod01.bic,arma_mod01.hqic)
+arma_mod21 = sm.tsa.ARMA(dta2.astype('float64'),(2,1)).fit()
+#print(arma_mod21.aic,arma_mod21.bic,arma_mod21.hqic)
+#arma_mod30 = sm.tsa.ARMA(dta2.astype('float64'),(3,0)).fit()
 #print(arma_mod30.aic,arma_mod30.bic,arma_mod30.hqic)
-#arma_mod40 = sm.tsa.ARMA(dta3,(7,1)).fit()
-#print(arma_mod40.aic,arma_mod40.bic,arma_mod40.hqic)
-#arma_mod50 = sm.tsa.ARMA(dta3,(8,0)).fit()
-#print(arma_mod50.aic,arma_mod50.bic,arma_mod50.hqic)
+#arma_mod31 = sm.tsa.ARMA(dta2.astype('float64'),(3,1)).fit()
+#print(arma_mod31.aic,arma_mod31.bic,arma_mod31.hqic)
 
+
+#174.55178448720673 185.280309396 178.901811661
+#206.098776562232 214.145170244 209.361296942
+#mod21-->#173.32772855124503 186.738384687 178.765262518
+#174.03232305944348 187.442979195 179.469857026
+#175.22216116088097 191.314948524 181.747201921
+
+#so the best is mod21
+
+resid = arma_mod21.resid 
+
+fig = plt.figure(figsize=(12,8))
+ax1 = fig.add_subplot(211)
+fig = sm.graphics.tsa.plot_acf(resid.values.squeeze(), lags=40, ax=ax1)
+ax2 = fig.add_subplot(212)
+fig = sm.graphics.tsa.plot_pacf(resid, lags=40, ax=ax2)
+
+print(sm.stats.durbin_watson(arma_mod21.resid.values))
+#1.94381530456
+
+print(stats.normaltest(resid))
+fig = plt.figure(figsize=(12,8))
+ax = fig.add_subplot(111)
+fig = qqplot(resid, line='q', ax=ax, fit=True)
+
+#Q/Jung-Box
+r,q,p = sm.tsa.acf(resid.values.squeeze(), qstat=True)
+data = np.c_[range(1,41), r[1:], q, p]
+table = pd.DataFrame(data, columns=['lag', "AC", "Q", "Prob(>Q)"])
+#print(table.set_index('lag'))
+
+#predict
+
+predict_dta = arma_mod21.predict(108, 208, dynamic=True)
+print(predict_dta)
+
+
+fig, ax = plt.subplots(figsize=(12, 8))
+ax = dta2.loc[dta2.index[0:]].plot(ax=ax)
+fig = arma_mod21.plot_predict(108, 208, dynamic=True, ax=ax, plot_insample=False)
+plt.show()
